0738749871c2b9742ec87555637b438bcfaeb2d1
Monica Hong <momo.star@gmail.com>
2021-05-26 06:16:40 -0700

    alreem's old card (:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Monica from "./components/Monica/Monica";
+import Alreem from "./components/Alreem/Alreem";
 function App() {
   return (
     <div className="flex">
       <Monica />
+      <Alreem />
     </div>
   );
 }
