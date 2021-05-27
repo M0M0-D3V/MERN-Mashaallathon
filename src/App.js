73bebc173308a988bc4facae636d8f2513e8566c
RawaAlsabbah <rawan.alsabbah1@gmail.com>
2021-05-27 14:48:49 +0300

    rawan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Monica from "./components/Monica/Monica";
 import Alreem from "./components/Alreem/Alreem";
+import Rawan from "./components/Rawan/Rawan";
+
 function App() {
   return (
     <div className="flex">
       <Monica />
       <Alreem />
+      <Rawan />
     </div>
   );
 }
---
+++ src/components/Rawan/Rawan.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Profile from "./MyComponents/Profile.jsx"
+import Info from "./MyComponents/Info.jsx"
+import style from "./Rawan.module.css"
+import Form from "./MyComponents/Form.jsx"
+
+const Rawan = () => {
+
+    return (
+    <div className={ style.card }>
+        <Profile />
+        <Info />
+        <Form />
+    
+    </div>
+    )
+}
+
+export default Rawan
